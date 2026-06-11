109b95dc0e98f428af43ff7842f4a481d13ebf5b
benjamin-s-hobbs <benjamin.s.hobbs@gmail.com>
2026-06-10 19:12:30 -0700

    changed 'dev-data-001' to 'dev-data-007'

--- terraform/consumers/dev/main.tf
+++ terraform/consumers/dev/main.tf
@@ -18,7 +18,7 @@ module "data_bucket" {
   project_label      = "cgep-lab"
   environment        = "dev"
   retention_days     = 30
-  bucket_name_suffix = "dev-data-001"
+  bucket_name_suffix = "dev-data-007"
 }
 
 output "attestation" { value = module.data_bucket.compliance_attestation }
