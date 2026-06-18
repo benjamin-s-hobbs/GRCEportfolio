273db814f873daa2c05e243c872244bae688f860
benjamin-s-hobbs <benjamin.s.hobbs@gmail.com>
2026-06-18 12:45:12 -0700

    Lab7-1: Capstone. editing outputs.tf (editing outputs) 'cgep-capstone'

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,13 @@
 # outputs.tf
-output "bucket_arn"         { value = aws_s3_bucket.primary.arn }
-output "bucket_name"        { value = aws_s3_bucket.primary.id }
+output "bucket_arn"         { value = aws_s3_bucket.uploads.arn }
+output "bucket_name"        { value = aws_s3_bucket.uploads.arn }
 output "log_bucket_arn"     { value = aws_s3_bucket.log.arn }
 output "kms_key_arn"        { value = aws_kms_key.key.arn }
 
 output "encryption_algorithm" {
   description = "Server-side encryption algorithm in effect (SC-28 attestation)."
   value = one([
-    for rule in aws_s3_bucket_server_side_encryption_configuration.primary.rule :
+    for rule in aws_s3_bucket_server_side_encryption_configuration.uploads.rule :
     rule.apply_server_side_encryption_by_default[0].sse_algorithm
   ])
 }
