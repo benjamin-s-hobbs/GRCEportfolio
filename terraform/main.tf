28d33114b7e1c5a84407a72303f41ca477cebede
benjamin-s-hobbs <benjamin.s.hobbs@gmail.com>
2026-06-22 19:02:28 -0700

    Lab7-1: Updating main.tf to refactor policy buckets for bcukets

--- terraform/main.tf
+++ terraform/main.tf
@@ -352,6 +352,33 @@ resource "aws_s3_bucket_versioning" "uploads" {
   }
 }
 
+data "aws_iam_policy_document" "uploads_secure_transport" {
+  statement {
+    sid    = "DenyInsecureTransport"
+    effect = "Deny"
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      aws_s3_bucket.uploads.arn,
+      "${aws_s3_bucket.uploads.arn}/*",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "uploads" {
+  bucket = aws_s3_bucket.uploads.id
+  policy = data.aws_iam_policy_document.uploads_secure_transport.json
+}
 
 # AC-3: Access control, explicit deny on every public access vector.
 resource "aws_s3_bucket_public_access_block" "uploads" {
